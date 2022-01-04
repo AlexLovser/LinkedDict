8fad6f8774fb7066f178874d68a362103a00bee6
Alex Lovser <tabalex2005@gmail.com>
2022-01-04 19:48:34 +0300

    # 5

---
+++ linked_dict/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: Alex Lovser
+:licence: Apache Licence, Version 2.0, see LICENSE file
+
+:copyright: (c) 2022 Alex Lovser
+"""
+
+from .linked_dict import LinkedDict
+
+__author__ = 'Alex Lovser'
+__version__ = '0.0.1'
+__email__ = 'tabalex2005@gmail.com'
