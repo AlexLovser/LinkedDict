2c0c3b3aadb35e0dc08f82c26192d5a68e403209
Alex Lovser <tabalex2005@gmail.com>
2022-01-05 00:15:55 +0300

    # Added ability to use any functions into

--- linked_dict/linked_dict.py
+++ linked_dict/linked_dict.py
@@ -2,43 +2,54 @@ from re import findall, sub
 from typing import Optional, SupportsInt, Union
 
 
-class _Node:
-	values = []
-	functions = []
-	countable = {}
-	graph_map = {}
-	pattern = r'\$\([A-zА-я0-9\-+*/%={}()\s\[\].,:\'"]+\)\$'
-	obj = None
+class Unknown:
+	def __repr__(self):
+		return repr(None)
 
-	class _Unknown:
-		def __repr__(self):
-			return repr(None)
+	def __str__(self):
+		return str(None)
 
-		def __str__(self):
-			return str(None)
 
-	class LinkedKeysError(ValueError):
-		pass
+class LinkedKeysError(ValueError):
+	pass
 
-	def _replace_defined(self, function) -> str:
-		return ''.join(str(repr(x) if (x := self.countable.get(part)) else part) for part in self._split(function))
 
-	def _find_expressions(self, string: Optional[str]) -> list:
-		return findall(self.pattern, string) if isinstance(string, str) else []
+class _LinkedValue:
+	def __init__(self, *, key, obj):
+		self.obj = obj
+		self.value = self.obj[key]
+		self.key = key
+		self.expressions_into = [
+			_LinkedFunction(parent=self, start=(start := self.value.index(exp)), end=start + len(exp))
+			for exp in self._find_expressions(self.value)
+		]
+
+		if not self.expressions_into:
+			self.obj.countable[key] = self.value
+
+		self.obj.values.append(self)
+		self.obj.functions.extend(self.expressions_into)
+
+	def __repr__(self):
+		return self.showing_value
+
+	def __str__(self):
+		return str(self.showing_value)
 
 	def _recheck(self, value):
-		if value.expressions_into and self.countable.get(value.key):
-			del value.countable[value.key]
+		if value.expressions_into and self.obj.countable.get(value.key):
+			del self.obj.countable[value.key]
 		else:
-			self.countable[value.key] = value.showing_value
+			self.obj.countable[value.key] = value.showing_value
 
-	def _deep(self, way: Union[list, tuple]):
-		lines = self.graph_map.get(way[-1], set())
-		if set(way) & set(lines):
-			trace = ' -> '.join(way + [way[0]])
-			raise self.LinkedKeysError(f'Your keys link themselves in loop! Trace is: {trace}')
-		for node in lines:
-			self._deep(way + [node])
+	def _replace_defined(self, function) -> str:
+		return ''.join(
+			str(repr(x) if (x := self.obj.countable.get(part)) else part)
+			for part in self._split(function)
+		)
+
+	def _find_expressions(self, string: Optional[str]) -> list:
+		return findall(self.obj.pattern, string) if isinstance(string, str) else []
 
 	@staticmethod
 	def _split(string: Optional[str]):
@@ -50,41 +61,13 @@ class _Node:
 				array[-1] += letter
 		return filter(None, array)
 
-	@staticmethod
-	def find_where(iterable: Union[list, tuple], *, key, count: Optional[SupportsInt] = None) -> Optional[list]:
-		if count is None:
-			count = len(iterable)
-		return [i for i in iterable if key(i) and (count := count - 1)]
-
-
-class _LinkedValue(_Node):
-	def __init__(self, *, key):
-		self.value = self.obj[key]
-		self.key = key
-		self.expressions_into = [
-			_LinkedFunction(parent=self, start=(start := self.value.index(exp)), end=start + len(exp))
-			for exp in self._find_expressions(self.value)
-		]
-
-		if not self.expressions_into:
-			self.countable[key] = self.value
-
-		self.values.append(self)
-		self.functions.extend(self.expressions_into)
-
-	def __repr__(self):
-		return self.showing_value
-
-	def __str__(self):
-		return str(self.showing_value)
-
 	@property
 	def showing_value(self):
-		return self.value if self.countable.get(self.key) else sub(self.pattern, '{}', str(self.value)).format(*self.expressions_into)
+		return self.value if self.obj.countable.get(self.key) else sub(self.obj.pattern, '{}', str(self.value)).format(*self.expressions_into)
 
 	def try_to_count(self):
 		for function in self.expressions_into:
-			if (set(self.countable) & function.expr_parts) == function.expr_parts:
+			if (set(self.obj.countable) & function.expr_parts) == function.expr_parts:
 				function.answer = self._replace_defined(function.content)
 				try:
 					function.answer = eval(function.answer)
@@ -94,22 +77,22 @@ class _LinkedValue(_Node):
 
 	def commit(self):
 		val = self.showing_value
-		if not sub(self.pattern, '', str(self.value)):
+		if not sub(self.obj.pattern, '', str(self.value)):
 			try:
 				val = eval(val)
 			except (NameError, TypeError, SyntaxError):
 				pass
 		for f in self.expressions_into:
-			if isinstance(f.answer, self._Unknown):
+			if isinstance(f.answer, Unknown):
 				break
 		else:
-			self.countable[self.key] = val
+			self.obj.countable[self.key] = val
 
-		self.obj[self.key] = val
+		self.obj.original[self.key] = val
 
 	def update(self, value):
 		for func in self.expressions_into:
-			self.functions.remove(func)
+			self.obj.functions.remove(func)
 
 		self.value = value
 
@@ -119,67 +102,68 @@ class _LinkedValue(_Node):
 		]
 
 		if self.expressions_into:
-			self.functions.extend(self.expressions_into)
+			self.obj.functions.extend(self.expressions_into)
 
 		self._recheck(self)
 
 		links = set()
 		for ex in self.expressions_into:
 			links = links.union(ex.expr_parts)
-		self.graph_map[self.key] = links
 
-		for i in self.expressions_into:
-			self._deep([i])
+		self.obj.graph_map[self.key] = links
+
+		for i in links:
+			self.obj.deep([i])
 
 		self.try_to_count()
 
 		def complete(value_key):
-			for val in self.values:
+			for val in self.obj.values:
 				save_it = False
 				for expr in val.expressions_into:
 					if value_key in expr.expr_parts:
-						expr.answer = self._Unknown()
+						expr.answer = Unknown()
 						save_it = True
+
 				if save_it:
 					self._recheck(val)
 					val.try_to_count()
 					complete(val.key)
-
 		complete(self.key)
 
 
-class _LinkedFunction(_Node):
+class _LinkedFunction:
 	def __init__(self, *, parent, start: int, end: int):
 		self.start, self.end = start, end
 		self.parent = parent
 		self.content = self.parent.value[start + 2:end - 2]
-		self.expr_parts = {i for i in findall(r'[A-zА-я][0-9A-zА-я]*', self.content) if self.obj.get(i)}
-		self.answer = self._Unknown() if self.expr_parts else eval(self.content)
+		self.expr_parts = {i for i in findall(r'[A-zА-я][0-9A-zА-я]*', self.content) if self.parent.obj.get(i)}
+		self.answer = Unknown() if self.expr_parts else eval(self.content)
 
 	def get_original(self):
 		return self.parent.value[self.start + 2:self.end - 2]
 
 	def __repr__(self):
-		return repr(self.answer if not isinstance(self.answer, self._Unknown) else self.get_original())
+		return repr(self.answer if not isinstance(self.answer, Unknown) else self.get_original())
 
 	def __str__(self):
-		return str(self.answer if not isinstance(self.answer, self._Unknown) else self.get_original())
+		return str(self.answer if not isinstance(self.answer, Unknown) else self.get_original())
 
 
-class LinkedDict(_Node):
-	def __init__(self, /, obj: Optional[dict], *, dynamic: Optional[bool] = True, debug: Optional[bool] = True):
-		if obj is None:
-			obj = {}
-
-		_Node.obj = obj
-
-		self.__obj = obj
-		self.__dynamic = dynamic
-		self.__debug = debug
+class LinkedDict:
+	def __init__(self, /, original: Optional[dict], *, dynamic: Optional[bool] = True, debug: Optional[bool] = True):
+		self.values = []
+		self.functions = []
+		self.countable = {}
 		self.graph_map = {}
+		self.pattern = r'\$\([A-zА-я0-9\-+*/%={}()\s\[\].,:\'"]+\)\$'
+		self.dynamic = dynamic
+		self.debug = debug
+		self.graph_map = {}
+		self.original = original if original else {}
 
-		for key, value in self.__obj.items():
-			v = _LinkedValue(key=key)
+		for key, value in self.original.items():
+			v = _LinkedValue(key=key, obj=self)
 			links = set()
 			for expr in v.expressions_into:
 				links = links.union(expr.expr_parts)
@@ -187,9 +171,9 @@ class LinkedDict(_Node):
 
 		if self.graph_map:
 			for i in self.graph_map:
-				self._deep([i])
+				self.deep([i])
 
-		x = len(self.__obj)
+		x = len(self.original)
 		while x > len(self.countable):
 			for value in self.values:
 				if not self.countable.get(value.key):
@@ -199,30 +183,52 @@ class LinkedDict(_Node):
 		return self == other
 
 	def __getitem__(self, item):
-		return self.__obj[item]
+		return self.original[item]
 
 	def __setitem__(self, key: str, value):
-		if self.__dynamic and (vals := self.find_where(self.values, key=lambda x: x.key == key)):
+		if self.dynamic and (vals := self.find_where(self.values, key=lambda x: x.key == key)):
 			vals[0].update(value)
 		else:
-			self.obj[key] = value
-			_LinkedValue(key=key).try_to_count()
+			self.original[key] = value
+			_LinkedValue(key=key, obj=self).try_to_count()
 
 	def __str__(self):
-		return str(self.__obj)
+		return str(self.original)
 
 	def __repr__(self):
-		return repr(self.__obj)
+		return repr(self.original)
 
 	def __iter__(self):
-		return self.__obj.__iter__()
+		return self.original.__iter__()
 
 	def items(self):
-		return self.__obj.items()
+		return self.original.items()
 
 	def get(self, value: str, default=None):
-		return self.__obj.get(value, default=default)
+		return self.original.get(value, default)
+
+	def deep(self, way: Union[list, tuple]):
+		lines = self.graph_map.get(way[-1], set())
+		# print('lines', lines)
+		if set(way) & set(lines):
+			trace = ' -> '.join(way + [way[0]])
+			raise LinkedKeysError(f'Your keys link themselves in loop! Trace is: {trace}')
+		for node in lines:
+			self.deep(way + [node])
+
+	@staticmethod
+	def find_where(iterable: Union[list, tuple], *, key, count: Optional[SupportsInt] = None) -> Optional[list]:
+		if count is None:
+			count = len(iterable)
+		return [i for i in iterable if key(i) and (count := count - 1)]
 
 
 if __name__ == '__main__':
+	d = LinkedDict(
+		{
+			'a': 5,
+			'b': '$(xx(a))$',
+		}
+	)
+	print(d)
 	print('(c) Made by Alex Lovser. Thanks for using!')
